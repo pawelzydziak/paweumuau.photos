c06782a3befc3b5ca8f85d50e02e6f78b58eaba8
Paweł Żydziak <pawel.zydziak@gmail.com>
2025-01-20 12:39:14 +0100

    start over

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,38 @@
+"use client";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-gray-300">
-            <div className="flex-1 flex justify-center items-center">
-                <img
-                    src="/photos/asdf.jpg"
-                    alt="Main Cover"
-                    className="object-cover w-full max-w-md shadow-md"
-                />
+    return (<div className="flex">
+
+            <div className="min-h-screen w-1/2 bg-[#222222] flex justify-center items-center">
+                <div className="flex flex-col max-w-xs">
+                    <div className="text-white py-3  text-5xl  font-bold font-sans  text-nowrap">Paweł Żydziak</div>
+                    <text className="text-white text-opacity-70 py-3  mb-10 font-light font-sans text-center">Inspirujący cytat.
+                    </text>
+                    <Link
+                        href="/fotografie"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md text-2xl font-semibold font-mono mb-4 text-center"
+                    >
+                        Fotografie
+                    </Link>
+                    <Link
+                        href="/muzyka"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md text-2xl font-semibold font-mono mb-4 text-center"
+                    >
+                        Muzyka
+                    </Link>
+                    <Link
+                        href="/kontakt"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md text-2xl font-semibold font-mono text-center"
+                    >
+                        Kontakt
+                    </Link>
+                </div>
+
             </div>
-            <div className="flex-1 flex flex-col gap-6 items-center">
-                <Link
-                    href="/fotografie"
-                    className="bg-white py-4 px-8 rounded-lg shadow-md hover:bg-gray-200 text-xl font-semibold"
-                >
-                    Fotografie
-                </Link>
-                <Link
-                    href="/muzyka"
-                    className="bg-white py-4 px-8 rounded-lg shadow-md hover:bg-gray-200 text-xl font-semibold"
-                >
-                    Muzyka
-                </Link>
-                <Link
-                    href="/kontakt"
-                    className="bg-white py-4 px-8 rounded-lg shadow-md hover:bg-gray-200 text-xl font-semibold"
-                >
-                    Kontakt
-                </Link>
+            <div className="w-1/2 max-h-screen">
+                <img className="object-cover w-full h-full" src="/photos/mainpage-cover.jpg" alt="cover"/>
             </div>
         </div>
     );
