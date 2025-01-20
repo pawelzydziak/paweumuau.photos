50cfbb000b004a208cf983fed008a03d72289fef
Paweł Żydziak <pawel.zydziak@gmail.com>
2025-01-20 14:01:06 +0100

    contact form wirefrrame

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,7 +1,9 @@
 export default function Kontakt() {
     return (
-        <div>
-            <h1>Strona kontakt</h1>
+        <div className="flex items-center justify-center bg-[#222222] w-full h-screen">
+            <div className="bg-white w-2/3 h-1/3 flex items-center justify-center text-center">
+                CONTACT FORM TO BE
+            </div>
         </div>
-    )
+    );
 }
