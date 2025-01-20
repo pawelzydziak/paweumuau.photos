cec3188a09476d405c54666dfd39f2dbb17c53c3
Paweł Żydziak <pawel.zydziak@gmail.com>
2025-01-20 14:34:49 +0100

    photos page separated

--- app/fotografie/page.tsx
+++ app/fotografie/page.tsx
@@ -1,17 +1,47 @@
-import AlbumGrid from "./AlbumGrid";
-
+import Link from "next/link";
 export default function Fotografie() {
-    const categories = ["polskie", "zagraniczne", "inne"];
-
     return (
-        <div className="min-h-screen p-8">
-        <h1 className="text-3xl font-bold text-center mb-8">Fotografie</h1>
-    {categories.map((category) => (
-        <div key={category} className="mb-12">
-    <h2 className="text-2xl font-semibold mb-4 capitalize">{category}</h2>
-        <AlbumGrid category={category} />
-    </div>
-    ))}
-    </div>
-);
+        <div className="flex flex-col md:flex-row h-screen">
+
+            <div className="w-full md:w-1/2 max-h-screen">
+                <img
+                    className="object-cover w-full h-full"
+                    src="/photos/meta/fotografie-cover.jpg"
+                    alt="cover"
+                />
+            </div>
+            <div className="flex flex-col w-full md:w-1/2 bg-[#222222] justify-center items-center p-10 space-y-8">
+                <div className="flex flex-col max-w-xs">
+                    <div className="text-white py-3  text-5xl  font-bold font-sans text-center text-nowrap">Portfolio</div>
+                    <text
+                        className="text-white text-opacity-70 py-3  mb-10 font-light font-sans text-center">Poniżej znajdują się przyciski odsyłające do stron z albumami.
+                    </text>
+                    <Link
+                        href="/fotografie/polska"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md  font-semibold font-mono text-center mb-8"
+                    >
+                        WYCIECZKI PO POLSCE
+                    </Link>
+                    <Link
+                        href="/fotografie/zagranica"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md font-semibold font-mono mb-8 text-center"
+                    >
+                        WYCIECZKI ZAGRANICZNE
+                    </Link>
+                    <Link
+                        href="/fotografie/inne"
+                        className="bg-stone-50 py-3 px-10 rounded-3xl shadow-md font-semibold font-mono mb-12 text-center"
+                    >
+                        INNE
+                    </Link>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    );
+
 }
